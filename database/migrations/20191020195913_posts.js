b07899c7f363611aa41ff6fe2da1e477c80c8039
Sara Alkhamri <s.alkhamri@gmail.com>
2019-10-23 12:39:31 -0700

    testing

--- database/migrations/20191020195913_posts.js
+++ database/migrations/20191020195913_posts.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('posts', tbl => {
         tbl.increments()
-        tbl.string('title', 128).
+        tbl.string('title', 128)
         tbl.string('category', 128)
         tbl.text('description')
         tbl.string('imgURL', 128)
