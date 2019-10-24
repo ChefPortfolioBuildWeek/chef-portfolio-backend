e44a1b91035e56cd6bfab0bafb8a979ff608045a
Sara Alkhamri <s.alkhamri@gmail.com>
2019-10-23 18:24:11 -0700

    updates

--- database/migrations/20191023122343_receipes.js
+++ database/migrations/20191023122343_receipes.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('receipes', tbl => {
         tbl.increments();
         tbl.string('title', 128).notNullable();
-        tbl.json('category').notNullable();
+        tbl.string('category').notNullable();
         tbl.text('description', 128).notNullable();
         tbl.string('imgURL', 128);
         tbl.string('username', 128);
