314267553ddb132b56fb39244a2b73193ccf1811
Sara Alkhamri <s.alkhamri@gmail.com>
2019-10-23 17:02:09 -0700

    testing with Ben!

--- database/migrations/20191020195913_posts.js
+++ database/migrations/20191020195913_posts.js
@@ -1,13 +1,13 @@
 
 exports.up = function(knex) {
     return knex.schema.createTable('posts', tbl => {
-        tbl.increments()
-        tbl.string('title', 128)
-        tbl.string('category', 128)
-        tbl.text('description')
-        tbl.string('imgURL', 128)
-        tbl.string('username', 128)
-        tbl.string('location', 128)
+        tbl.increments();
+        tbl.string('title', 128);
+        tbl.string('category', 128);
+        tbl.text('description');
+        tbl.string('imgURL', 128);
+        tbl.string('username', 128);
+        tbl.string('location', 128);
     })
 };
 
