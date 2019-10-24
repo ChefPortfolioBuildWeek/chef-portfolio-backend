63d08d1f497377159f6458a98626d872e9738a6a
Sara Alkhamri <s.alkhamri@gmail.com>
2019-10-24 10:35:19 -0700

    Added notNullable back to posts migration. Issue preventing info to post on pg fixed

--- database/migrations/20191020195913_posts.js
+++ database/migrations/20191020195913_posts.js
@@ -2,9 +2,9 @@
 exports.up = function(knex) {
     return knex.schema.createTable('posts', tbl => {
         tbl.increments();
-        tbl.string('title', 128);
-        tbl.string('category', 128);
-        tbl.text('description');
+        tbl.string('title', 128).notNullable();
+        tbl.string('category', 128).notNullable();
+        tbl.text('description').notNullable();
         tbl.string('imgURL', 128);
         tbl.string('username', 128);
         tbl.string('location', 128);
