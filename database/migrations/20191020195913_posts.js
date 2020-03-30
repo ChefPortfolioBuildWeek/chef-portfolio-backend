1fc3c523e6b85c397940d6e6907984024cdec48c
vpagano10 <vpagano10@gmail.com>
2020-03-30 15:53:10 -0400

    Trying to make amount of allowed characters for the image url for recipies longer but having migration issues.

--- database/migrations/20191020195913_posts.js
+++ database/migrations/20191020195913_posts.js
@@ -1,16 +1,15 @@
-
 exports.up = function(knex) {
-    return knex.schema.createTable('posts', tbl => {
-        tbl.increments();
-        tbl.string('title', 128).notNullable();
-        tbl.string('category', 128).notNullable();
-        tbl.text('description').notNullable();
-        tbl.string('imgURL', 128);
-        tbl.string('username', 128);
-        tbl.string('location', 128);
-    })
+  return knex.schema.createTable("posts", tbl => {
+    tbl.increments();
+    tbl.string("title", 128).notNullable();
+    tbl.string("category", 128).notNullable();
+    tbl.text("description").notNullable();
+    tbl.string("imgURL", 1000);
+    tbl.string("username", 128);
+    tbl.string("location", 128);
+  });
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('posts');
+  return knex.schema.dropTableIfExists("posts");
 };
